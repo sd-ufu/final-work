96b2adc36c87928f67e4215ec54fa76b41d6b667
Ivan Guimarães Monte <ivg.monte@gmail.com>
2020-11-24 18:55:28 -0300

    Creating database proto file

---
+++ src/main/java/sdufu/finalwork/grpc/document/Document.java
@@ -0,0 +1,31 @@
+package sdufu.finalwork.grpc.document;
+
+public class Document {
+	private long version;
+	private long timestamp;
+	private byte[] data;
+
+	public long getVersion() {
+		return version;
+	}
+
+	public void setVersion(long version) {
+		this.version = version;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public byte[] getData() {
+		return data;
+	}
+
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+}
---
+++ src/main/java/sdufu/finalwork/grpc/document/Table.java
@@ -0,0 +1,8 @@
+package sdufu.finalwork.grpc.document;
+
+import java.math.BigInteger;
+import java.util.Hashtable;
+
+public class Table extends Hashtable<BigInteger, Document> {
+	private static final long serialVersionUID = 1L;
+}
