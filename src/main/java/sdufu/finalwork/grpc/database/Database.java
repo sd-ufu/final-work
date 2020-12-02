eeac9c84a516b1ceca98f953d56065e48950e8c9
Ivan Guimarães Monte <ivg.monte@gmail.com>
2020-12-01 22:12:34 -0300

    Removing unecessary code

--- src/main/java/sdufu/finalwork/grpc/database/Database.java
+++ src/main/java/sdufu/finalwork/grpc/database/Database.java
@@ -10,13 +10,4 @@ import sdufu.finalwork.grpc.database.model.Document;
  */
 public class Database extends Hashtable<BigInteger, Document> {
 	private static final long serialVersionUID = 1L;
-	private static BigInteger KEY = new BigInteger("1");
-
-	public static synchronized BigInteger generateKey() {
-		BigInteger current = new BigInteger(Database.KEY.toString());
-
-		Database.KEY.add(new BigInteger("1"));
-
-		return current;
-	}
 }
